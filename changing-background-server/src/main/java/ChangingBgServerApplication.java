4b1ebfee86512f926cd557417e4003947571c27d
mac-changing <changings_mail@163.com>
2020-03-17 21:56:16 +0800

    complete bg-server Application

--- changing-background-server/src/main/java/ChangingBgServerApplication.java
+++ changing-background-server/src/main/java/ChangingBgServerApplication.java
@@ -1,7 +1,25 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
 /**
  * @author chenjun
  * @version V1.0
  * @since 2020-03-17 13:18
  */
-public class ChangingBgServerApplication {
+@SpringBootApplication
+@ComponentScan("com.changing.bg")
+public class ChangingBgServerApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChangingBgServerApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ChangingBgServerApplication.class);
+    }
+
 }
--- changing-background-server/src/main/java/com/changing/bg/controller/LoginController.java
+++ changing-background-server/src/main/java/com/changing/bg/controller/LoginController.java
@@ -1,9 +1,28 @@
 package com.changing.bg.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author chenjun
  * @version V1.0
  * @since 2020-03-17 13:19
  */
+@RestController
 public class LoginController {
+
+    @GetMapping("/test")
+    public Map<String, String> test() {
+        Map<String, String> map = new HashMap<>();
+        map.put("time", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        map.put("username", "test");
+        map.put("fileType", "txt");
+
+        return map;
+    }
 }
