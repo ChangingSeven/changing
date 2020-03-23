c14677aff6ce505bfdd99f35a2338fe6a937c809
mac-changing <changings_mail@163.com>
2020-03-24 00:53:41 +0800

    router config

--- changing-background-front/src/router/index.js
+++ changing-background-front/src/router/index.js
@@ -1,15 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import IndexPage from '@/components/IndexPage'
+import LoginPage from '@/components/login/LoginPage'
+import NotFound from '@/components/framework/NotFound'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    mode: 'history',
+    base: '/bg-front',
+    routes: [
+        {
+            path: '/index',
+            name: 'IndexPage',
+            component: IndexPage,
+            meta: {
+                title: '首页'
+            }
+        }, {
+            path: '/login',
+            name: 'LoginPage',
+            component: LoginPage,
+            meta: {
+                title: '登录'
+            }
+        },
+        {
+            path: '*',
+            name: 'NotFound',
+            component: NotFound,
+            meta: {
+                title: '404 - Not Found'
+            }
+        }
+    ]
 })
