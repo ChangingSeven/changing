90e3fda4a5df83eb53a1bf9f9fafa85970e9095b
mac-changing <changings_mail@163.com>
2020-03-23 22:06:48 +0800

    remove SpringBootApplication annotation

--- changing-background-server/src/main/java/ChangingBgServerApplication.java
+++ changing-background-server/src/main/java/ChangingBgServerApplication.java
@@ -1,17 +1,18 @@
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author chenjun
  * @version V1.0
  * @since 2020-03-17 13:18
  */
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
 @EnableAutoConfiguration
 @ComponentScan("com.changing.bg")
 @MapperScan("com.changing.bg.mapper")
