06bbf07c57dbd93b22edb9398a61ecf4ce8b23b1
mac-changing <changings_mail@163.com>
2020-03-24 00:54:54 +0800

    import useful modules

--- changing-background-front/src/main.js
+++ changing-background-front/src/main.js
@@ -4,7 +4,25 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+import axios from 'axios'
+Vue.use(axios)
+
+import qs from 'qs'
+Vue.use(qs)
+
 Vue.config.productionTip = false
+
+router.beforeEach((to, from, next) => {
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = to.meta.title;
+  }
+  next();
+})
 
 /* eslint-disable no-new */
 new Vue({
